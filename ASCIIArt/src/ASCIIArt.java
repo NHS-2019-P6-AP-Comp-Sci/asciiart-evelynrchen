71f25f8243d628623c9a0dfd9fb12296845909c2
s-chenev <s-chenev@53S50030831.it.bsd405.org>
2019-09-20 13:24:49 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -17,11 +17,60 @@
 // YOU CODE HERE :) 
 
 public class ASCIIArt {
+	public static void twoCandles() {
+		System.out.println("                                           /\\       /\\");
+		System.out.println("                                          |__|     |__|");
+		candleSide();
+		candleSide();
+		candleSide();
+	}
+	
+	public static void candleSide() {
+		System.out.println("                                           ||       ||");
+	}
+
+	public static void layerOne() {
+		System.out.print("                                       --------------------");
+		System.out.print("\n                                      |\\       /  \\      / |");
+		System.out.println("\n                                      | \\     /    \\    /  |");
+		System.out.println("                                      |  \\   /      \\  /   |");
+		System.out.println("                                      |   ___        __    |");
+		layerOneSide();
+		layerOneSide();
+	}
+	
+	public static void layerOneSide() {
+		System.out.println("                                      |                    |");
+	}
+	
+	public static void layerTwo() {
+		System.out.println("                      ------------------------------------------------------");
+		System.out.println("                      |       OO      OO        OOOOOO          OOOO        |");
+		System.out.println("                      |       OO      OO        OO   OO         O  OO       |");
+		System.out.println("                      |       OO      OO        OO   O          O    OO     |");
+		System.out.println("                      |       OOOOOOOOOO        OOOOO           O     OO    |");
+		System.out.println("                      |       OO      OO        OO  O           O     OO    |");
+		System.out.println("                      |       OO      OO        OO   OO         O    OO     |");
+		System.out.println("                      |       OO      OO        OOOOOO          OOOOOO      |");
+		}
+	
+	public static void layerThree() {
+		System.out.println("            ----------------------------------------------------------------------------");
+		System.out.println("            |           \\           /     \\            /     \\           /             |");
+		System.out.println("            |            \\         /       \\          /       \\         /              |");
+		System.out.println("            |             \\       /         \\        /         \\       /               |");
+		System.out.println("            |              \\     /           \\      /           \\     /                |");
+		System.out.println("            |               \\   /             \\    /             \\   /                 |");
+		System.out.println("            |                __                 __                 __                  |");
+		System.out.println("            ----------------------------------------------------------------------------");
+	}
+	
+	
 	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+		twoCandles();
+		layerOne();
+		layerTwo();
+		layerThree();
+		System.out.println("                                           Happy Birthday!");
 	}
 }
